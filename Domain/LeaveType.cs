9186c5adb501f156ee235c9b7bf69240b1395616
Dylan Lopes <dylananlopes@gmail.com>
2024-02-01 08:41:34 +0000

    First commit - created domain project

---
+++ Domain/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common;
+
+namespace Domain
+{
+  public class LeaveType : BaseEntity
+  {
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+  }
+}
