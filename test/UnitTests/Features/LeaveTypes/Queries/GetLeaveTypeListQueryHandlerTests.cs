228ae227ea14efb09e43db4ef2f931b71e3248d0
Dylan Lopes <dylananlopes@gmail.com>
2024-05-13 09:22:18 +0100

    Unit tests

---
+++ test/UnitTests/Features/LeaveTypes/Queries/GetLeaveTypeListQueryHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using Application.Interfaces.Persistence;
+using Application.MappingProfiles;
+using AutoMapper;
+using Moq;
+using UnitTests.Mocks;
+
+namespace UnitTests.Features.LeaveTypes.Queries
+{
+  public class GetLeaveTypeListQueryHandlerTests
+  {
+    private readonly Mock<ILeaveTypeRepository> _mockRepo;
+    private readonly IMapper _mapper;
+
+    public GetLeaveTypeListQueryHandlerTests()
+    {
+      _mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+
+      var mapperConfig = new MapperConfiguration(c =>
+      {
+        c.AddProfile<LeaveTypeProfile>();
+      });
+
+      _mapper = mapperConfig.CreateMapper();
+    }
+
+    [Fact]
+    public async Task GetLeaveTypes_ShouldReturnThreeObjects()
+    {
+      var handler = new GetLeaveTypesQueryHandler(_mapper, _mockRepo.Object);
+      var result = await handler.Handle(new GetLeaveTypesQuery(), CancellationToken.None);
+
+      Assert.NotNull(result);
+      Assert.IsType<List<LeaveTypeDto>>(result);
+      Assert.True(result.Count == 3);
+    }
+  }
+}
