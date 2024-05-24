cf3a5957c87385e477314998144d8cc6b45fcc00
Dylan Lopes <dylananlopes@gmail.com>
2024-05-24 08:47:03 +0100

    Implemented UserService

---
+++ Infrastructure/Identity/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Application.Exceptions;
+using Application.Interfaces.Identity;
+using Application.Models.Identity;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography.Xml;
+
+namespace Identity.Services
+{
+  public class UserService : IUserService
+  {
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UserService(UserManager<ApplicationUser> userManager)
+    {
+      _userManager = userManager;
+    }
+
+    public async Task<List<Employee>> GetEmployees()
+    {
+      var users = await _userManager.GetUsersInRoleAsync("Employee");
+
+      return users.Select(u => new Employee()
+      {
+        Id = u.Id,
+        FirstName = u.FirstName,
+        LastName = u.LastName,
+        Email = u.Email
+      }).ToList();
+    }
+
+    public async Task<Employee> GetEmployeeById(string id)
+    {
+      var user = await _userManager.FindByIdAsync(id);
+
+      return new Employee()
+      {
+        Id = user.Id,
+        FirstName = user.FirstName,
+        LastName = user.LastName,
+        Email = user.Email
+      };
+    }
+  }
+}
---
+++ src/core/Application/Interfaces/Identity/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Application.Models.Identity;
+
+namespace Application.Interfaces.Identity
+{
+  public interface IUserService
+  {
+    Task<List<Employee>> GetEmployees();
+    Task<Employee> GetEmployeeById(int id);
+  }
+}
