e7816cbdf8b62213e01837d23cee927c11cb6692
Dylan Lopes <dylananlopes@gmail.com>
2024-10-16 09:07:40 +0100

    Added identityDbContext & service registration

--- src/core/infrastructure/Persistance/Repositories/GenericRepository.cs
+++ src/core/infrastructure/Persistance/Repositories/GenericRepository.cs
@@ -2,46 +2,40 @@
 using Domain.Common;
 using Microsoft.EntityFrameworkCore;
 using Persistance.DataBaseContext;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Persistance.Repositories
+namespace Persistance.Repositories;
+
+public class GenericRepository<T>(HrDbContext context) : IGenericRepository<T> where T : BaseEntity
 {
-    public class GenericRepository<T>(HrDbContext context) : IGenericRepository<T> where T : BaseEntity
+  protected readonly HrDbContext _context = context;
+
+  public async Task CreateAsync(T entity)
   {
-    protected readonly HrDbContext _context = context;
+    await _context.AddAsync(entity);
+    await _context.SaveChangesAsync();
+  }
 
-    public async Task CreateAsync(T entity)
-    {
-      await _context.AddAsync(entity);
-      await _context.SaveChangesAsync();
-    }
+  public async Task DeleteAsync(T entity)
+  {
+    _context.Remove(entity);
+    await _context.SaveChangesAsync();
+  }
 
-    public async Task DeleteAsync(T entity)
-    {
-      _context.Remove(entity);
-      await _context.SaveChangesAsync();
-    }
+  public async Task<IReadOnlyList<T>> GetAsync()
+  {
+    return await _context.Set<T>().AsNoTracking().ToListAsync();
+  }
 
-    public async Task<IReadOnlyList<T>> GetAsync()
-    {
-      return await _context.Set<T>().AsNoTracking().ToListAsync();
-    }
+  public async Task<T> GetByIdAsync(int id)
+  {
+    return await _context.Set<T>()
+                         .AsNoTracking()
+                         .FirstOrDefaultAsync(q => q.Id == id);
+  }
 
-    public async Task<T> GetByIdAsync(int id)
-    {
-      return await _context.Set<T>()
-        .AsNoTracking()
-        .FirstOrDefaultAsync(q => q.Id == id);
-    }
-
-    public async Task UpdateAsync(T entity)
-    {
-      _context.Entry(entity).State = EntityState.Modified;
-      await _context.SaveChangesAsync();
-    }
+  public async Task UpdateAsync(T entity)
+  {
+    _context.Entry(entity).State = EntityState.Modified;
+    await _context.SaveChangesAsync();
   }
 }
