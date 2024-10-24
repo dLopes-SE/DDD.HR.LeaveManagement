00b6f7d759be225d793c05f2950f2552b58e5cbe
Dylan Lopes <dylananlopes@gmail.com>
2024-10-24 09:50:56 +0100

    fixed LeaveTypesController

--- src/api/Api/Controllers/LeaveTypesController.cs
+++ src/api/Api/Controllers/LeaveTypesController.cs
@@ -23,7 +23,7 @@ namespace Api.Controllers
     {
       var leaveTypes = await _mediator.Send(new GetLeaveTypesQuery());
       if (leaveTypes.Count is 0)
-        return NoContent()
+        return NoContent();
 
       return Ok(leaveTypes);
     }
